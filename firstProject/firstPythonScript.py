eb7c2eda1224b391b32a57669d28d3beb8a070e2
domsqas-git <domsqas@gmail.com>
2021-03-16 13:33:17 -0400

    update

--- firstProject/firstPythonScript.py
+++ firstProject/firstPythonScript.py
@@ -1,2 +1,4 @@
 print("hello world")
 print("mi hai rotto i coglioni" ", " "fan 'culo")
+myStr= "Hello"
+messg = myStr + 12
--- helloPython.py
+++ helloPython.py
@@ -1,1 +1,11 @@
 print("Hello world")
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+pph = input("Enter Rate:")
+r = float(pph)
+
+if h<= 40 :
+    print(h * r)
+else :
+    print(((h-40)*1.5*r) + (40*r))
