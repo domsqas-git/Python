e3bf87072c3e2031a66bc2221b512917114b1c8e
domsqas-git <domsqas@gmail.com>
2021-03-25 20:05:23 -0400

    numnew

---
+++ num.py
@@ -0,0 +1,17 @@
+"""x = 1
+y = 1000
+
+def num_name ():
+    for in range (x, y):
+
+    if int [x] => 1:
+        return 1
+    print("one")
+    else:
+    print(-1)
+"""
+num = int(input("Type a number up 1K"))
+
+if 1 <= num <= 1000:
+    
+    
